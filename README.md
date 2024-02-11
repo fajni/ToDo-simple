b96665b4ba23c7405cf6e56f95a4be16b7e686d7
Veljko Fajnisevic <keychiper.cy@gmail.com>
2024-02-11 15:55:46 +0100

    Tests For Obligations

--- README.md
+++ README.md
@@ -5,9 +5,11 @@ Simple CRUD ToDo Application Made In .NET Framework Using Windows Forms. Main Id
 
 ```
 ├── source/TODO				# Contains all the source files for application.
+│   │
 │   ├── BusinessLayer			# Business logic for manipulating data, manage communications between repository and presentation layers.
 │   │   ├── UserBusiness.cs
 │   │   └── ObligationBusiness.cs
+│   │
 │   ├── PresentationLayer		# Contains user interface and logic responsible for presenting data to user, as well as user actions.
 │   │   ├── Resources			# Contains symbols/images used in presentation layer.
 │   │   ├── Program.cs
@@ -16,14 +18,19 @@ Simple CRUD ToDo Application Made In .NET Framework Using Windows Forms. Main Id
 │   │   ├── ObligationsForm.cs
 │   │   ├── RegistrationForm.cs
 │   │   └── UserForm.cs
+│   │
 │   ├── RepositoryLayer			# Data access layer that contains files for communication with the database.
 │   │   ├── UserRepository.cs
 │   │   ├── ObligationRepository.cs
 │   │   └── Constants.cs		# Contains connection string for database.
-│   └── Shared				# Folder that contains other content.
-│       ├── Models			# Available entities.
-│       ├── Interfaces			
-│       └── Tables			# Contains SQL files for creating tables and other SQL queries.
+│   │
+│   ├── Shared				# Folder that contains other content.
+│   │   ├── Models			# Available entities.
+│   │   ├── Interfaces			
+│   │   └── Tables			# Contains SQL files for creating tables and other SQL queries.
+│   │
+│   └── Tests				# Unit Testing
+│
 ├── ER					# Contains ER diagrams.
 │   └── entity-relationship diagram.png
 └── README.md                   	# Project documentation (you are here!)
---
+++ source/TODO/Tests/ObligationBusinessTests.cs
@@ -0,0 +1,86 @@
+﻿using BusinessLayer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tests
+{
+    [TestClass]
+    public class ObligationBusinessTests
+    {
+        private ObligationBusiness obligationBusiness = new ObligationBusiness();
+        private Obligation obligation = new Obligation
+        {
+            GetSetUserId = 1,
+            GetSetTitle = "Unit Test Obligation",
+            GetSetDate = DateTime.Now,
+            GetSetNote = "Note For Unit Test Obligation"
+        };
+        private List<Obligation> obligations = new List<Obligation>();
+
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            obligationBusiness = new ObligationBusiness();
+            Console.WriteLine(obligationBusiness.GetType());
+            Assert.AreEqual(obligationBusiness.GetType(), new ObligationBusiness().GetType());
+        }
+
+        [TestMethod]
+        public void GetAllObligationsTest()
+        {
+            obligations = obligationBusiness.GetAllObligations();
+            Assert.IsNotNull(obligations);
+        }
+
+        [TestMethod]
+        public void GetObligationsByUserIdTest1()
+        {
+            obligations = obligationBusiness.GetObligationsByUserId(1);
+            int numberOfObligationsForUser= obligationBusiness.GetAllObligations().Count - obligations.Count;
+            Assert.IsTrue(numberOfObligationsForUser>0);
+        }
+
+        [TestMethod]
+        public void GetObligationsByUserIdTest2()
+        {
+            Assert.IsNotNull(obligationBusiness.GetObligationsByUserId(1));
+        }
+
+        [TestMethod]
+        public void InsertObligationTest()
+        {
+            Assert.IsTrue(obligationBusiness.InsertObligation(obligation));
+        }
+
+        [TestMethod]
+        public void DeleteObligationTest()
+        {
+            obligationBusiness.InsertObligation(obligation);
+            Obligation newObligation = obligationBusiness.GetAllObligations().Where(o => o.GetSetTitle == obligation.GetSetTitle).ToList()[0];
+            Assert.IsTrue(obligationBusiness.DeleteObligation(newObligation.GetSetId));
+        }
+
+        [TestMethod]
+        public void UpdateObligationTest()
+        {
+            obligationBusiness.InsertObligation(obligation);
+            Obligation newObligation = obligationBusiness.GetAllObligations().Where(o => o.GetSetTitle == obligation.GetSetTitle).ToList()[0];
+            Assert.IsTrue(obligationBusiness.UpdateObligation(newObligation));
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            foreach(var o in obligationBusiness.GetAllObligations())
+            {
+                if(o.GetSetTitle == obligation.GetSetTitle)
+                {
+                    obligationBusiness.DeleteObligation(o.GetSetId);
+                }
+            }
+        }
+    }
+}
